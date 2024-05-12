0cd6c3a2bb8a89b77081f8172274bcd2b982e721
Yahia_ezzahri <yahiaezzahri@gmail.com>
2024-05-12 15:52:56 +0100

    ChatUI Implementation

--- chat/src/client/ChatUI.java
+++ chat/src/client/ChatUI.java
@@ -1,5 +1,293 @@
 package client;
 
-public class ChatUI {
-  
+import java.rmi.RemoteException;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.border.Border;
+
+public class ChatUI extends JFrame implements ActionListener{
+  private static final long serialVersionUID = 1L;	
+	private JPanel textPanel, inputPanel;
+	private JTextField textField;
+	private String name, message;
+	private Font meiryoFont = new Font("Meiryo", Font.PLAIN, 14);
+	private Border blankBorder = BorderFactory.createEmptyBorder(10,10,20,10);
+	private ChatClient chatClient;
+  private JList<String> list;
+  private DefaultListModel<String> listModel;
+    
+  protected JTextArea textArea, userArea;
+  protected JFrame frame;
+  protected JButton privateMsgButton, startButton, sendButton;
+  protected JPanel clientPanel, userPanel;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]){
+		
+		try{
+			for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
+				if("Nimbus".equals(info.getName())){
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		}
+		catch(Exception e){
+			}
+		new ChatUI();
+		}
+	
+	public ChatUI(){
+			
+		frame = new JFrame("Salle de discussion");	
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+		
+    @Override
+		  public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		        
+		    	if(chatClient != null){
+			    	try {
+			        	sendMessage("Au revoir à tous, je pars");
+			        	chatClient.IServer.quitChat(name);
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}		        	
+		        }
+		        System.exit(0);  
+		    }   
+		});
+
+		Container c = getContentPane();
+		JPanel outerPanel = new JPanel(new BorderLayout());
+		
+		outerPanel.add(getInputPanel(), BorderLayout.CENTER);
+		outerPanel.add(getTextPanel(), BorderLayout.NORTH);
+		
+		c.setLayout(new BorderLayout());
+		c.add(outerPanel, BorderLayout.CENTER);
+		c.add(getUsersPanel(), BorderLayout.WEST);
+
+		frame.add(c);
+		frame.pack();
+		frame.setAlwaysOnTop(true);
+		frame.setLocation(150, 150);
+		textField.requestFocus();
+	
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
+	/**
+	 * @return
+	 */
+
+	public JPanel getTextPanel(){
+		String welcome = "Bienvenue, entrez votre nom et appuyez sur Démarrer pour commencer\n";
+		textArea = new JTextArea(welcome, 14, 34);
+		textArea.setMargin(new Insets(10, 10, 10, 10));
+		textArea.setFont(meiryoFont);
+		
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		textPanel = new JPanel();
+		textPanel.add(scrollPane);
+	
+		textPanel.setFont(new Font("Meiryo", Font.PLAIN, 14));
+		return textPanel;
+	}
+	
+	/**
+	 * @return inputPanel
+	 */
+	public JPanel getInputPanel(){
+		inputPanel = new JPanel(new GridLayout(1, 1, 5, 5));
+		inputPanel.setBorder(blankBorder);	
+		textField = new JTextField();
+		textField.setFont(meiryoFont);
+		inputPanel.add(textField);
+		return inputPanel;
+	}
+
+	/**
+	 * @return
+	 */
+	public JPanel getUsersPanel(){
+		
+		userPanel = new JPanel(new BorderLayout());
+		String  userStr = " Utilisateurs actuels     ";
+		
+		JLabel userLabel = new JLabel(userStr, JLabel.CENTER);
+		userPanel.add(userLabel, BorderLayout.NORTH);	
+		userLabel.setFont(new Font("Meiryo", Font.PLAIN, 16));
+
+		String[] noClientsYet = {"Aucun autre utilisateur"};
+		setClientPanel(noClientsYet);
+
+		clientPanel.setFont(meiryoFont);
+		userPanel.add(makeButtonPanel(), BorderLayout.SOUTH);		
+		userPanel.setBorder(blankBorder);
+
+		return userPanel;		
+	}
+
+	/**
+	 * @param currClients
+	 */
+    public void setClientPanel(String[] currClients) {  	
+    	clientPanel = new JPanel(new BorderLayout());
+        listModel = new DefaultListModel<String>();
+        
+        for(String s : currClients){
+        	listModel.addElement(s);
+        }
+        if(currClients.length > 1){
+        	privateMsgButton.setEnabled(true);
+        }
+
+        list = new JList<String>(listModel);
+        list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        list.setVisibleRowCount(8);
+        list.setFont(meiryoFont);
+        JScrollPane listScrollPane = new JScrollPane(list);
+
+        clientPanel.add(listScrollPane, BorderLayout.CENTER);
+        userPanel.add(clientPanel, BorderLayout.CENTER);
+    }
+	
+	/**
+	 * @return
+	 */
+	public JPanel makeButtonPanel() {		
+		sendButton = new JButton("Envoyer ");
+		sendButton.addActionListener(this);
+		sendButton.setEnabled(false);
+
+        privateMsgButton = new JButton("Msg prv");
+        privateMsgButton.addActionListener(this);
+        privateMsgButton.setEnabled(false);
+		
+		startButton = new JButton("Commencer ");
+		startButton.addActionListener(this);
+		
+		JPanel buttonPanel = new JPanel(new GridLayout(4, 1));
+		buttonPanel.add(privateMsgButton);
+		buttonPanel.add(new JLabel(""));
+		buttonPanel.add(startButton);
+		buttonPanel.add(sendButton);
+		
+		return buttonPanel;
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e){
+
+		try {
+			//get connected to chat service
+			if(e.getSource() == startButton){
+				name = textField.getText();				
+				if(name.length() != 0){
+					frame.setTitle(name + "'salle de discussion ");
+					textField.setText("");
+					textArea.append("utilisateur : " + name + " connexion à...\n");							
+					getConnected(name);
+					if(!chatClient.connectionProblem){
+						startButton.setEnabled(false);
+						sendButton.setEnabled(true);
+						}
+				}
+				else{
+					JOptionPane.showMessageDialog(frame, "Entrer votre nom");
+				}
+			}
+
+			//get text and clear textField
+			if(e.getSource() == sendButton){
+				message = textField.getText();
+				textField.setText("");
+				sendMessage(message);
+				System.out.println("message en cours : " + message);
+			}
+			
+			//send a private message, to selected users
+			if(e.getSource() == privateMsgButton){
+				int[] privateList = list.getSelectedIndices();
+				
+				for(int i=0; i<privateList.length; i++){
+					System.out.println("index selectionné :" + privateList[i]);
+				}
+				message = textField.getText();
+				textField.setText("");
+				sendPrivate(privateList);
+			}
+			
+		}
+		catch (RemoteException remoteExc) {			
+			remoteExc.printStackTrace();	
+		}
+		
+	}//end actionPerformed
+
+	// --------------------------------------------------------------------
+	
+	/**
+	 * @param message
+	 * @throws RemoteException
+	 */
+	private void sendMessage(String message) throws RemoteException {
+		chatClient.IServer.updateChat(name, message);
+	}
+
+	/**
+	 * Send a message, to be relayed, only to selected chatters
+	 * @param chatMessage
+	 * @throws RemoteException
+	 */
+	private void sendPrivate(int[] privateList) throws RemoteException {
+		String privateMessage = "[Privé de " + name + "] :" + message + "\n";
+		chatClient.IServer.privateDM(privateList, privateMessage);
+	}
+	
+	/**
+	 * Make the connection to the chat server
+	 * @param userName
+	 * @throws RemoteException
+	 */
+	private void getConnected(String userName) throws RemoteException{
+		//remove whitespace and non word characters to avoid malformed url
+		String cleanedUserName = userName.replaceAll("\\s+","_");
+		cleanedUserName = userName.replaceAll("\\W+","_");
+		try {		
+			chatClient = new ChatClient(this, cleanedUserName);
+			chatClient.startClient();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
 }
